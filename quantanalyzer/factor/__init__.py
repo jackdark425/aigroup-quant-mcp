c033a7bf740d6049fb8f9f6a816469b75456b728
jackdark425 <jackdark425@users.noreply.github.com>
2025-10-21 10:19:36 +0800

    update

--- quantanalyzer/factor/__init__.py
+++ quantanalyzer/factor/__init__.py
@@ -4,11 +4,10 @@ Factor module for quantitative analysis
 
 from .library import FactorLibrary
 from .evaluator import FactorEvaluator
-from .alpha158 import Alpha158Generator, get_alpha158_config
+from .alpha158 import Alpha158Generator
 
 __all__ = [
     'FactorLibrary',
     'FactorEvaluator',
-    'Alpha158Generator',
-    'get_alpha158_config'
+    'Alpha158Generator'
 ]
